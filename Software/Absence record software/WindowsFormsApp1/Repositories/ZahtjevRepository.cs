ac097d3660cd8050961d9d506ada8cde1936d457
pknezovic21 <pknezovic21@student.foi.hr>
2023-05-22 09:40:38 +0200

    Dodani prazni repositories

---
+++ Software/Absence record software/WindowsFormsApp1/Repositories/ZahtjevRepository.cs
@@ -0,0 +1,14 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Models;
+
+namespace WindowsFormsApp1.Repositories {
+    public class ZahtjevRepository {
+    
+    }
+}
