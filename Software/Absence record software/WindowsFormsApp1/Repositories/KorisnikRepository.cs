750a4cf016186943a118841e5d1a20b17f2d3ca4
pknezovic21 <pknezovic21@student.foi.hr>
2023-05-22 14:27:36 +0200

    Kreirane funkcije za KorisnikRepository

--- Software/Absence record software/WindowsFormsApp1/Repositories/KorisnikRepository.cs
+++ Software/Absence record software/WindowsFormsApp1/Repositories/KorisnikRepository.cs
@@ -1,10 +1,71 @@
-﻿using System;
+﻿using DBLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsFormsApp1.Models;
 
 namespace WindowsFormsApp1.Repositories {
     public class KorisnikRepository {
+
+        public static Korisnik DohvatiKorisnika(string email) {
+            string sql = $"SELECT * FROM Korisnik WHERE Email = '{email}'";
+            return Dohvati(sql);
+        }
+        public static Korisnik DohvatiKorisnika(int id) {
+            string sql = $"SELECT * FROM Korisnik WHERE IdKorisnika = {id}";
+            return Dohvati(sql);
+        }
+
+        private static Korisnik Dohvati(string sql) {
+
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            Korisnik korisnik = null;
+            if (reader.HasRows == true) {
+                reader.Read();
+                korisnik = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return korisnik;
+        }
+
+        public static List<Korisnik> DohvatiKorisnike() {
+            var korisnici = new List<Korisnik>();
+
+            string sql = "SELECT * FROM Korisnik";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read()) {
+                Korisnik korisnik = CreateObject(reader);
+                korisnici.Add(korisnik);
+            }
+
+            reader.Close();
+            DB.CloseConnection();
+
+            return korisnici;
+        }
+
+        private static Korisnik CreateObject(System.Data.SqlClient.SqlDataReader reader) {
+            int id = int.Parse(reader["IdKorisnika"].ToString());
+            string ime = reader["Ime"].ToString();
+            string prezime = reader["Prezime"].ToString();
+            string lozinka = reader["Lozinka"].ToString();
+            string mail = reader["Email"].ToString();
+            int idUloge = Convert.ToInt32(reader["IdUloge"]);
+            var korisnik = new Korisnik {
+                IdKorisnika = id,
+                Ime = ime,
+                Prezime = prezime,
+                Lozinka = lozinka,
+                Email = mail,
+                IdUloge = idUloge
+            };
+
+            return korisnik;
+        }
     }
 }
